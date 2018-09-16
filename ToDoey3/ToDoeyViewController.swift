1dc8936289f7ec80a5aa1f9f6593fdcf2c1cc5ae
Dyary Bayz <dyaryraoofbayz@dyarys-mac.local>
2018-09-16 03:28:27 -0700

    CheckMarks are complete

---
+++ ToDoey3/ToDoeyViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ToDoey3
+//
+//  Created by Dyary Raoof Bayz on 9/15/18.
+//  Copyright © 2018 Dyary Raoof Bayz. All rights reserved.
+//
+
+import UIKit
+
+class ToDoeyViewController: UITableViewController {
+
+    let itemArray = ["Dyary", "Raoof", "Bayz"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoeyCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+       
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print (itemArray[indexPath.row])
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
